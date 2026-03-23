5d8436f5c26425716437058314658dd66aac88c0
atharv1345 <atharvk1345@gmail.com>
2026-03-23 17:04:15 +0530

    Fix privacy toggle disabled logic using campaign.isPublic

--- src/pages/campaign/CampaignEdit.tsx
+++ src/pages/campaign/CampaignEdit.tsx
@@ -195,7 +195,7 @@ const CampaignEditForm = ({
             disabled={loading || disabled}
           />
         }
-        disabled={disableOnPrivate && !initialCampaignCreate.isPublic}
+        disabled={disableOnPrivate && !campaign.isPublic}
         sx={{ my: 2 }}
         label={
           <Typography variant="body1" color="textSecondary">
